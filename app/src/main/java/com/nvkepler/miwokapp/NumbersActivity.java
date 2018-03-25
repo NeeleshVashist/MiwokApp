0ab88b6bf3685e494664b605b53875ffd45d3c11
nvkepler <neeleshvashist@gmail.com>
2018-03-25 20:48:29 +0530

    Add : ArrayList

--- app/src/main/java/com/nvkepler/miwokapp/NumbersActivity.java
+++ app/src/main/java/com/nvkepler/miwokapp/NumbersActivity.java
@@ -1,6 +1,11 @@
 package com.nvkepler.miwokapp;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by neelv on 21/3/18.
@@ -13,17 +18,35 @@ public class NumbersActivity extends MainActivity {
         setContentView(R.layout.activity_numbers);
 
         // Create an array of words
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+//        String[] words = new String[10];
+//        words[0] = "one";
+//        words[1] = "two";
+//        words[2] = "three";
+//        words[3] = "four";
+//        words[4] = "five";
+//        words[5] = "six";
+//        words[6] = "seven";
+//        words[7] = "eight";
+//        words[8] = "nine";
+//        words[9] = "ten";
 
+        //ArrayList of Words
+        ArrayList<String> words = new ArrayList<>();
+        words.add(0,"one");
+        words.add(1,"two");
+        words.add(2,"three");
+        words.add(3,"four");
+        words.add(4,"five");
+        words.add(5,"six");
+        words.add(6,"seven");
+        words.add(7,"eight");
+        words.add(8,"nine");
+        words.add(9,"ten");
+
+        //Log msg to print a log at a particular point
+        //Log.d("word", "word at 0 index : " + words.get(0));
+
+        //Log to print all the elements in the arraylist
+        //Log.d("word","Words : " + words);
     }
 }
